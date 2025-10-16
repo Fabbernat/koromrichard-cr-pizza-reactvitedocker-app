a42aa905fb124caa9e48bfe6d465924b94f9de7e
fabbernat <fabbernat@gmail.com>
2025-10-16 15:08:26 +0200

    fix based on the other project

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,27 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import type { Pizza } from './types/Pizza'
-import { apiClient, BACKEND_URL } from './api/ApiClient'
+import { apiClient, BACKEND_URL } from './api/ApiClient' // az `apiClient` kimozdítható a {}-en kívülre
+
 function App() {
   const [pizzak, setPizzak] = useState<Array<Pizza>>([])
-  const [pizza, setPizza] = useState<Pizza | null>(null)
 
   useEffect(() => {
     apiClient.
-    get('/pizzak')
+      get('/pizzak')
       .then((response) => setPizzak(response.data))
       .catch((result) => console.error(result))
   }, [])
 
   return (
     <>
-    <p>HELLÓ!!!</p>
+      <p>HELLÓ!!!</p>
       {pizzak.map((pizza) => (
         <div key={pizza.id}>
           <h2>{pizza.nev}</h2>
           <p>{pizza.leiras}</p>
           <p>{pizza.ar} Ft</p>
-          <img src={`${BACKEND_URL}/kepek/${pizza.imageUrl}`} alt={pizza.nev} width={200} />
+          <img src={`${BACKEND_URL}/kepek/${pizza.imageUrl}`} alt={pizza.nev} width={300} />
         </div>
       ))}
     </>
