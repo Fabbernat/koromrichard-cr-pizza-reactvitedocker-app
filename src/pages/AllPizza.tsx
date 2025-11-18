c8ca572bf1033d6f125c6970ec5ab8069c71fdbc
rkorom <richard.korom@gmail.com>
2025-11-18 09:10:58 +0100

    Add Bootstrap cards

--- src/pages/AllPizza.tsx
+++ src/pages/AllPizza.tsx
@@ -3,6 +3,7 @@ import apiClient, { baseURL } from "../api/apiClient";
 import type { Pizza } from "../types/Pizza";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 const AllPizza = () => {
   const navigate = useNavigate();
@@ -12,39 +13,36 @@ const AllPizza = () => {
   useEffect(() => {
     apiClient
       .get("/pizzak")
-      .then((Response) => setPizzak(Response.data))
+      .then((response) => setPizzak(response.data))
       .catch(() => toast.error("A pizzák betöltése sikertelen volt"));
   }, []);
 
+  const generateCard = (p: Pizza) => {
+    return (
+      <Col>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={`${baseURL}/kepek/${p.imageUrl}`} />
+          <Card.Body>
+            <Card.Title>{p.nev}</Card.Title>
+            <Card.Text>{p.leiras}</Card.Text>
+            <Button
+              onClick={() => navigate(`/pizza/${p.id}`)}
+              variant="success"
+            >
+              Megtekintés
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  };
+
   return (
-    <>
-      <table>
-        <thead>
-          <th>Név</th>
-          <th>Leírás</th>
-          <th>Ár</th>
-          <th>Kép</th>
-          <th>Megtekintés</th>
-        </thead>
-        <tbody>
-          {pizzak.map((p) => (
-            <tr>
-              <td>{p.nev}</td>
-              <td>{p.leiras}</td>
-              <td>{p.ar}</td>
-              <td>
-                <img width={100} src={`${baseURL}/kepek/${p.imageUrl}`} />
-              </td>
-              <td>
-                <button onClick={() => navigate(`/pizza/${p.id}`)}>
-                  Megtekintés
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+    <Container>
+      <Row xs={"auto"} md={"auto"} className="g-4">
+        {pizzak.map((i) => generateCard(i))}
+      </Row>
+    </Container>
   );
 };
 
